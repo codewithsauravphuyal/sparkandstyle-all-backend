4b3b7bcf64aa20faaf5864580d342a78e0d4fe07
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2026-03-10 12:21:06 +0545

    chore: stop tracking dist build output

--- dist/utils/generateToken.d.ts
+++
@@ -1,4 +0,0 @@
-import jwt from 'jsonwebtoken';
-export declare const generateToken: (userId: string) => string;
-export declare const verifyToken: (token: string) => string | jwt.JwtPayload;
-//# sourceMappingURL=generateToken.d.ts.map
